40086773b83d5590bfcf36d88cfad6d2bd829aa2
Adharsh Jolly <ajcodesofficial+github@gmail.com>
2025-03-03 15:53:58 +0530

    Login Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen
+        name="login"
+        options={{ title: "Login", headerShown: false }}
+      />
       <Stack.Screen name="sign-up" options={{ title: "Sign Up" }} />
       <Stack.Screen name="index" options={{ title: "Dashboard" }} />
       <Stack.Screen name="profile" options={{ title: "Profile" }} />
